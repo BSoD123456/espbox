a116a6877b24e2df4d769cba7a469a5bb60e4ea2
BSoD123456 <bsod123456@gmail.com>
2026-07-28 21:17:00 +0800

    standard commit

---
+++ main/sys/ebx_ui.h
@@ -0,0 +1,6 @@
+#ifndef __INC_EBX_UI_H__
+#define __INC_EBX_UI_H__
+
+void ebx_ui_init(void);
+
+#endif /*__INC_EBX_UI_H__*/
---
+++ main/sys/ui.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "esp_err.h"
+#include "esp_log.h"
+
+#include "ebx_ui.h"
+
+//static const char* TAG = "ebx_ui";
+
+void ebx_ui_init(void) {
+
+}
+
