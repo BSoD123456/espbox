e54ab6a18cd9f9a787a0d471d0463f1eef3a287c
BSoD123456 <bsod123456@gmail.com>
2026-08-31 18:40:04 +0800

    standard commit

--- main/espbox.c
+++ main/espbox.c
@@ -1,15 +1,93 @@
 #include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+
+#include "esp_err.h"
+#include "esp_log.h"
 
 #include "ebx_sys.h"
 #include "ebx_app.h"
 
+static char* TAG = "ebx_main";
+
+#define MAX_INNER_ENTRIES   1
+
+const char* g_root = "/storage";
+
+static int find_surfix(const char* dst, const char* sub) {
+    size_t dlen = strlen(dst);
+    size_t slen = strlen(sub);
+    int didx = dlen - slen;
+    if(didx < 0) {
+        return -1;
+    }
+    if(memcmp(dst + didx, sub, slen) == 0) {
+        return didx;
+    } else {
+        return -1;
+    }
+}
+
+static int match_file(int didx, const char* surfix, char** p_rname) {
+    DIR* root = opendir(g_root);
+    if(!root) {
+        ESP_LOGE(TAG, "open root failed");
+        abort();
+    }
+    struct dirent* ent;
+    int mcnt = 0;
+    char* rname = NULL;
+    while( (ent = readdir(root)) != NULL ) {
+        if(ent->d_type == DT_REG) continue;
+        int sfidx = find_surfix(ent->d_name, surfix);
+        if(sfidx < 0 || mcnt++ < didx) continue;
+        size_t rlen = strlen(g_root) + strlen(ent->d_name) + 2;
+        rname = malloc(rlen);
+        snprintf(rname, rlen, "%s/%s", g_root, ent->d_name);
+        break;
+    }
+    closedir(root);
+    *p_rname = rname;
+    return mcnt;
+}
+
 void app_main(void) {
     ebx_nvs_init();
-    uint8_t pwflags = ebx_nvs_get_u8("power_flags");
-    printf("pwflags 0x%x\n", pwflags);
-    ebx_nvs_set_u8("power_flags", 1);
-
     ebx_fs_init();
+    uint8_t pwflags = ebx_nvs_get_u8("power_flags");
+    int stidx = (int)ebx_nvs_get_u8("power_start_idx");
+    int ostidx = stidx;
+    if(pwflags == 1) {
+        stidx++;
+    } else {
+        ebx_nvs_set_u8("power_flags", 1);
+    }
+    char* dfname;
+    int midx = match_file(stidx, ".gbc.zip", &dfname);
+    if(midx < stidx) {
+        while(stidx - midx > MAX_INNER_ENTRIES) {
+            stidx -= midx + MAX_INNER_ENTRIES;
+        }
+    }
+        if(stidx != ostidx) {
+            if(stidx > 0xff || stidx < 0) {
+                ESP_LOGE(TAG, "invalid power_start_idx");
+            }
+            ebx_nvs_set_u8("power_start_idx", (uint8_t)stidx);
+        }
+        if(stidx > midx) {
+            ESP_LOGI(TAG, "enter inner entry: %d", stidx - midx);
+            return;
+        } else if(/*TODO*/) {
+            assert(dfname == NULL);
+            midx = match_file(stidx, ".gbc.zip", &dfname);
+            assert(midx == stidx);
+        }
+    assert(dfname != NULL);
+    ESP_LOGI(TAG, "enter file entry: %s", dfname);
+    free(dfname);
+    return;
+
     ebx_disp_init();
     ebx_ipt_init();
 #if 1
