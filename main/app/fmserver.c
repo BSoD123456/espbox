bc3a274692cba1a270ec9900ed6445a94172dca0
BSoD123456 <bsod123456@gmail.com>
2026-09-01 20:41:48 +0800

    standard commit

--- main/app/fmserver.c
+++ main/app/fmserver.c
@@ -32,8 +32,8 @@
 
 /* Max size of an individual file. Make sure this
  * value is same as that set in upload_script.html */
-#define MAX_FILE_SIZE   (200*1024) // 200 KB
-#define MAX_FILE_SIZE_STR "200KB"
+#define MAX_FILE_SIZE   (2*1024*1024)
+#define MAX_FILE_SIZE_STR "2MB"
 
 /* Scratch buffer size */
 #define SCRATCH_BUFSIZE  8192
