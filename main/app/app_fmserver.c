51d484a3c9281d5133aebbcce6a801858a535d47
BSoD123456 <bsod123456@gmail.com>
2026-09-01 20:09:10 +0800

    standard commit

--- main/app/app_fmserver.c
+++ main/app/app_fmserver.c
@@ -8,11 +8,11 @@
 #include "app.h"
 
 #define APP_NAME fmserver
-//static const char* TAG = "ebx_app_fmserver";
+static const char* TAG = "ebx_app_fmserver";
 
 esp_err_t example_start_file_server(const char *base_path);
 
 REG_APP {
-    printf(params[0]);
+    ESP_LOGI(TAG, "start file manage http server");
     example_start_file_server("/storage");
 }
